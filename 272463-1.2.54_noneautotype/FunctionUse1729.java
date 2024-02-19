db3daf144a1142ee8acc93a95956e15204be4368
duzhg56980 <guokey1996@gmail.com>
2024-02-19 10:54:21 +0800

    init commit

---
+++ 272463-1.2.54_noneautotype/FunctionUse1729.java
@@ -0,0 +1,6 @@
+import com.alibaba.fastjson.support.spring.PropertyPreFilters;
+public class FunctionUse1729 {
+public void funcUse() {
+PropertyPreFilters propertyprefilters = new PropertyPreFilters();
+}
+}
---
+++ 272463-1.2.54_noneautotype/FunctionUse1975.java
@@ -0,0 +1,6 @@
+import com.alibaba.fastjson.JSONPath.RefOpSegement;
+public class FunctionUse1975 {
+public void funcUse() {
+RefOpSegement refopsegement = new RefOpSegement();
+}
+}
